2022a954a214add7867fb679d289256d0b9c18f5
nonetype-123 <objectobject039@gmail.com>
2024-07-18 09:43:30 +0300

    Create stupid_calculator.cpp

---
+++ stupid_calculator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "a: ";
+  cin >> a;
+  
+  cout << "b: ";
+  cin >> b;
+
+  int total = a + b;
+
+  cout << total;
+
+  return 0;
+}
